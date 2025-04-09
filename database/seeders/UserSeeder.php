9ebafc1fe47ed85e1169f3704bb67f5ce9a05a55
cuongthieu-itme <thieutrancuong.me@gmail.com>
2025-04-09 11:58:04 +0700

    config role migrate

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin Center',
+            'email' => 'admin_center@gmail.com',
+            'password' => Hash::make('123123'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Teacher Center',
+            'email' => 'teacher_center@gmail.com',
+            'password' => Hash::make('123123'),
+            'role' => 'teacher',
+        ]);
+
+        User::create([
+            'name' => 'User Center',
+            'email' => 'user_center@gmail.com',
+            'password' => Hash::make('123123'),
+            'role' => 'student',
+        ]);
+    }
+}
